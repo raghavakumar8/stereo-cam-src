5fff8dd42908920b65059114bd62b560138a087e
Lab User <Lab User>
2017-05-10 14:35:45 -0400

    Changed MATLAB census function to use vector based windowing to match verilog code

--- MATLAB/census_match.m
+++ MATLAB/census_match.m
@@ -19,8 +19,8 @@ function disparity_map = census_match(left, right, maxdisp)
 % left = l1;
 % right = r1;
 
-left = conv2(left, ones(5,5), 'same');
-right = conv2(right, ones(5,5), 'same');
+%left = conv2(left, ones(5,5), 'same');
+%right = conv2(right, ones(5,5), 'same');
 
 [m n]=size(left);
 
@@ -40,8 +40,9 @@ corrSumKernel =window'*window; %should probably be made two separate
                                %1-D convs
 
 %do the census transform
-leftCen = zeros(m-windowSize+1, n-windowSize+1, windowSize*windowSize);
-rightCen = zeros(m-windowSize+1, n-windowSize+1, windowSize*windowSize);
+leftCen = zeros(m-windowSize+1, n-windowSize+1, 3*3);
+rightCen = zeros(m-windowSize+1, n-windowSize+1, 3*3);
+leftCenVis = zeros(m-windowSize+1, n-windowSize+1);
 
 %Generate the censur matrix for both left and right images
 for y=1:m-windowSize+1
@@ -55,8 +56,38 @@ for y=1:m-windowSize+1
                 currentPixL = left(y+j*2, x+i*2);
                 currentPixR = right(y+j*2, x+i*2);
                 %if pixel > center pixel, then 1, else 0
-                leftCen(y, x, j*9 + i + 1) = currentPixL > centerPixL;
-                rightCen(y, x, j*9 + i + 1) = currentPixR > centerPixR;
+                leftCen(y, x, j*(windowSize+1)/2 + i + 1) = currentPixL > centerPixL;
+                rightCen(y, x, j*(windowSize+1)/2 + i + 1) = currentPixR > centerPixR;
+            end
+        end
+    end
+end
+
+leftCenVis = 128*leftCen(:,:,1) + 64*leftCen(:,:,2) + 32*leftCen(:,:,3) ...
+    + 16*leftCen(:,:,4) + 8*leftCen(:,:,6) + 4*leftCen(:,:,7) ... 
+    + 2*leftCen(:,:,8) + leftCen(:,:,9);
+figure;imagesc(leftCenVis);colormap(gray);axis image;
+
+%do the census windowing
+[cen_m, cen_n, cen_l] = size(leftCen);
+leftCenWnd = zeros(cen_m-postCensusWindowSize+1, cen_n-postCensusWindowSize+1, 72);
+rightCenWnd = zeros(cen_m-postCensusWindowSize+1, cen_n-postCensusWindowSize+1, 72);
+
+%Generate the censur matrix for both left and right images
+for y=1:cen_m-postCensusWindowSize+1
+    for x=1:cen_n-postCensusWindowSize+1
+        % Combine into a single long ass 72 bit vec
+        for j=0:(postCensusWindowSize-1)
+            for i=0:(postCensusWindowSize-1)
+                CenL = leftCen(y + j, x + i,:);
+                CenR = rightCen(y + j, x + i,:);
+                % Remove the center census bit
+                CenL(5) = [];
+                CenR(5) = [];
+                % Now the census bit vec is 8 bits long
+                
+                leftCenWnd(y, x, (j*24 + i*8 + 1):(j*24 + i*8 + 8)) = CenL;
+                rightCenWnd(y, x, (j*24 + i*8 + 1):(j*24 + i*8 + 8)) = CenR;
             end
         end
     end
@@ -65,13 +96,14 @@ end
 h = waitbar(0,'Computing disparity...');
 set(h,'Name','Disparity progress');
 
+[cenwnd_m, cenwnd_n, cenwnd_l] = size(leftCenWnd);
 for d=0:maxdisp
     diss(:) = Inf;
     
-    for y = 1:m-windowSize+1
-        for x = 1:n-windowSize+1-d
-            leftCenVec = leftCen(y, x+d, :);
-            rightCenVec = rightCen(y, x, :);
+    for y = 1:cenwnd_m
+        for x = 1:cenwnd_n-d
+            leftCenVec = leftCenWnd(y, x+d, :);
+            rightCenVec = rightCenWnd(y, x, :);
             
             hammingVec = xor(leftCenVec, rightCenVec);
             hammingDist = sum(hammingVec);
@@ -80,8 +112,8 @@ for d=0:maxdisp
         end
     end
     
-    img(:,:,d+1) = conv2(diss,corrSumKernel,'same');
-    %img(:,:,d+1) = diss;
+    %img(:,:,d+1) = conv2(diss,corrSumKernel,'same');
+    img(:,:,d+1) = diss;
     waitbar(d/maxdisp);
 end
 
